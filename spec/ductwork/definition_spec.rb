bcc8bb10695c9851115cd20f21daeeb8a00ddb64
Tyler Ewing <tewing10@gmail.com>
2025-01-05 06:37:26 -0600

    chore: add basic test for `Ductwork::Definition`

--- spec/ductwork/definition_spec.rb
+++ spec/ductwork/definition_spec.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 
-RSpec.describe Ductwork::Definition
+RSpec.describe Ductwork::Definition do
+  describe "#steps" do
+    it "returns the collection of steps" do
+      steps = described_class.new.steps
+
+      expect(steps).to be_empty
+    end
+  end
+end
