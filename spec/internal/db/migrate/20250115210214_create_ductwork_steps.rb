a5ffe5c4c9653242dbf66ae3b3323298b9e0819b
Tyler Ewing <tewing10@gmail.com>
2025-01-20 06:06:44 -0600

    fix: commit generated files in test rails app
    this was causing CI to fail because ductwork's railtie runs before the
    test rails app initializer that generates the files

---
+++ spec/internal/db/migrate/20250115210214_create_ductwork_steps.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class CreateDuctworkSteps < ActiveRecord::Migration[7.0]
+  def change
+    create_table :ductwork_steps do |table|
+      table.belongs_to :pipeline, index: true, null: false, foreign_key: { to_table: :ductwork_pipelines }
+      table.belongs_to :next_step, index: true, foreign_key: { to_table: :ductwork_steps }
+      table.string :step_type, null: false
+      table.string :klass, null: false
+      table.string :status, null: false
+      table.timestamp :started_at
+      table.timestamp :completed_at
+    end
+  end
+end
