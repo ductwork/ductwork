58c7b1c00a082db3e97ae18346865f8f6e6060ec
Tyler Ewing <tewing10@gmail.com>
2025-10-17 08:47:08 -0500

    chore: add more pipeline definition specs
    as noted in the file, these tests create more complex pipelines to weed
    out bugs

---
+++ spec/ductwork/pipeline_definitions_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+# NOTE: this test may eventually be absorbed into branch and/or deinition
+# builder specs. this test file is meant to exercise more complex pipeline
+# definitions to uncover any bugs
+RSpec.describe "Pipeline definitions" do # rubocop:disable RSpec/DescribeClass
+  it "correctly chains steps after dividing" do
+    definition = Class.new(Ductwork::Pipeline) do
+      define do |pipeline|
+        pipeline.start(MyFirstJob)
+        pipeline.divide(to: [MySecondJob, MyThirdJob]) do |branch1, branch2|
+          branch1.chain(MyFourthJob)
+          branch1.combine(branch2, into: MyFifthJob)
+        end
+      end
+    end.pipeline_definition
+
+    branch1, branch2 = definition.branch.children
+    combined_branch = definition.branch.children.first.children.sole
+    expect(branch1.steps.length).to eq(2)
+    expect(branch1.steps.second.klass).to eq(MyFourthJob)
+    expect(branch1.children.length).to eq(1)
+    expect(branch2.steps.length).to eq(1)
+    expect(branch2.children.length).to eq(1)
+    expect(combined_branch.steps.sole.klass).to eq(MyFifthJob)
+  end
+
+  it "correctly handles combining multiple branches" do
+    definition = Class.new(Ductwork::Pipeline) do
+      define do |pipeline|
+        pipeline.start(MyFirstJob)
+        pipeline.divide(to: [MySecondJob, MyThirdJob]) do |branch1, branch2|
+          branch1.divide(to: [MyFourthJob, MyFifthJob]) do |sub_branch1, sub_branch2|
+            branch2.combine(sub_branch1, sub_branch2, into: MyFirstJob)
+          end
+        end
+      end
+    end.pipeline_definition
+
+    branch1, branch2 = definition.branch.children
+    sub_branch1, sub_branch2 = branch1.children
+    combined_branch = branch2.children.sole
+    expect(combined_branch.parents).to contain_exactly(branch2, sub_branch1, sub_branch2)
+    expect(sub_branch1.children).to match_array(combined_branch)
+    expect(sub_branch2.children).to match_array(combined_branch)
+  end
+end
