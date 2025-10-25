8f869dc428fc4e2746a14ee35081e35d42d72d6f
Tyler Ewing <tewing10@gmail.com>
2025-10-24 20:42:42 -0500

    feat: add "advancing" status enum to `Step` model

--- app/models/ductwork/step.rb
+++ app/models/ductwork/step.rb
@@ -14,6 +14,7 @@ module Ductwork
     enum :status,
          pending: "pending",
          in_progress: "in_progress",
+         advancing: "advancing",
          completed: "completed"
 
     enum :step_type,
