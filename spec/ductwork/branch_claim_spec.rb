e14ebf018cc756ea0295c4861bf43cc99442ae0b
Tyler Ewing <tewing10@gmail.com>
2026-07-27 07:01:25 -0500

    fix: use deterministic in-step status for transition in spec

--- spec/ductwork/branch_claim_spec.rb
+++ spec/ductwork/branch_claim_spec.rb
@@ -144,7 +144,11 @@ RSpec.describe Ductwork::BranchClaim do
       end
 
       context "when there are orphaned advancements" do
-        let(:transition) { create(:transition, branch:) }
+        let(:transition) do
+          in_step = create(:step, :advancing, branch:)
+
+          create(:transition, branch:, in_step:, out_step: nil)
+        end
         let(:advancement) { create(:advancement, transition:) }
 
         before do
